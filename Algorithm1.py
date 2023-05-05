cd4c71e15c2f719f1c8208b93019198c2ee6ac98
shimonjacobs <93395451+shimonjacobs@users.noreply.github.com>
2023-05-05 09:04:26 +0100

    Update Algorithm1.py

--- Algorithm1.py
+++ Algorithm1.py
@@ -1,5 +1,7 @@
 
 # import libraries
+
+import time
 import cv2 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,9 +13,11 @@ cap = cv2.VideoCapture(0)
 while True:
     ret,frame = cap.read()
     
-    if cv2.waitKey(1) == ord('q'):
+   if gpio.input(4) == 0: #if gpio pin is low, stop recording
         break
-    
+        
+    #start frame timer
+    start_time = time.time()
     #convert to grayscale
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     
@@ -78,6 +82,31 @@ while True:
     cv2.imshow('frame', temp_img2)
     # cv2.imshow('frame', frame) for debugging purposes
     
+    #send direction instructions to arduino
+    turning_value1 = np.arctan(poly1[1]) * 180 / np.pi
+    turning_value2 = np.arctan(poly2[1]) * 180 / np.pi
+    turning_value = int((turning_value1 + turning_value2) / 2)
+    turning_value = int((turning_value + 90) * 255 / 180)
+    turning_value = turning_value.to_bytes(1, byteorder='big')
+
+    #convert turning value to wheel speed values
+    #wheel speed values are sent to arduino as a byte array
+
+    left_speed_value_1 = 255 - turning_value
+    right_speed_value_2 = 255 + turning_value
+    speed_value = speed_value.to_bytes(1, byteorder='big')
+
+    #send speed values to arduino using pwm through the gpio pins
+    left_speed_value_1 = left_speed_value_1.to_bytes(1, byteorder='big')
+    right_speed_value_2 = right_speed_value_2.to_bytes(1, byteorder='big')
+    
+    #end time
+    end_time = time.time()
+    time_elapsed=start_time-end_time
+    #record time into csv file
+    with open('time.csv', 'a') as f:
+        f.write(str(end_time - start_time) + '\n')
+       
 
 cap.release() #end video capture
 
